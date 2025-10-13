3f32cd5817835b4dc9ffc997053635b749899e26
Alan Tan <dodieboy@users.noreply.github.com>
2025-10-13 19:44:44 +0800

    .

--- apps/web/src/router/routes.js
+++ apps/web/src/router/routes.js
@@ -8,9 +8,10 @@ import PostAssignment from "../views/PostAssignment.vue"
 import AssignmentDetail from "../views/AssignmentDetail.vue"
 import Login from "../views/Login.vue"
 import Register from "../views/Register.vue"
-import { onAuthStateChanged, getAuth } from "firebase/auth"
-
 import Calendar from "../views/Calendar.vue"
+import Logout from "../views/Logout.vue"
+import { ref } from "vue"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
 
 const routes = [
   {
@@ -60,6 +61,11 @@ const routes = [
     component: Login,
   },
   {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
+  },
+  {
     path: "/register",
     name: "Register",
     component: Register,
@@ -84,26 +90,40 @@ const router = createRouter({
   },
 })
 
-const getCurrentUser = () => {
-  return new Promise((resolve, reject) => {
-    // Use the observer to get the current user and then unsubscribe immediately
-    const removeListener = onAuthStateChanged(
-      getAuth(),
-      (user) => {
-        removeListener(); // Clean up the observer
-        resolve(user);
-      },
-      reject
-    );
-  });
+//check if there is user session
+export const LoginStatus = ref(false);
+export const checkLoginStatus = () => {
+  if(localStorage.getItem('user') == null) return false
+  return true
+}
+
+export const getCurrentUser = async () => {
+  try{
+    return new Promise((resolve, reject) => {
+      // Use the observer to get the current user and then unsubscribe immediately
+      const removeListener = onAuthStateChanged(
+        getAuth(),
+        (user) => {
+          removeListener(); // Clean up the observer
+          resolve(user);
+        },
+        reject
+      );
+    });
+  } catch (error){
+    console.error("Error getting currentUser:", error)
+    return false;
+  }
 };
 
 // Navigation guard (check if login)
 router.beforeEach(async (to, from, next) => {
   // Check if the route requires authentication
-  console.log(to);
+  LoginStatus.value = checkLoginStatus();
+  console.log(LoginStatus)
   if (to.matched.some(record => record.meta.requiresAuth)) {
     const user = await getCurrentUser();
+    console.log(user);
     if (user) {
       next();
     } else {
---
+++ apps/web/src/views/Logout.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import { logoutUser, signInWithGoogle } from '../services/firebase'
+
+const router = useRouter()
+
+const handleLogout = async () => {
+  try {
+    const response = await logoutUser()
+
+    if (!response.success) {
+      alert(`Logout failed: ${response.error}`)
+      return
+    }
+
+    const user = response.user
+    localStorage.clear();
+
+    console.log('Logout successful:', user)
+    alert('Logout successful!')
+    router.push('/')
+
+  } catch (error) {
+    console.error('Logout error:', error)
+    alert('Unexpected error during logout.')
+  }
+}
+handleLogout()
+</script>
+
+
+<template>
+</template>
